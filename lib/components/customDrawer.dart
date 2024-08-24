20928ce0ffb9ebd2cbfe2ae9f355d04c8734909e
Divyansh3012 <84275199+Divyansh3012@users.noreply.github.com>
2024-08-24 21:46:35 +0530

    home , drawer UI

---
+++ lib/components/customDrawer.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Colors.grey,
+      child: Column(
+        children: [
+          // header
+          DrawerHeader(child: Icon(Icons.account_circle_rounded, size: 40,)),
+
+          //title
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0 ),
+            child: ListTile(
+              leading: Icon(Icons.home),
+              title: Text("H O M E"),
+              onTap: (){
+                Navigator.pop(context);
+              },
+            ),
+          ),
+
+
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0 ),
+            child: ListTile(
+              leading: Icon(Icons.account_circle_rounded),
+              title: Text("P R O F I L E"),
+              onTap: (){
+                Navigator.pop(context);
+              },
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0 ),
+            child: ListTile(
+              leading: Icon(Icons.add),
+              title: Text("A D D  B L O G"),
+              onTap: (){
+                Navigator.pop(context);
+              },
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0 ),
+            child: ListTile(
+              leading: Icon(Icons.logout),
+              title: Text("L O G O U T"),
+              onTap: (){
+                Navigator.pop(context);
+              },
+            ),
+          ),
+
+          
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/profile.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Username")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.person,
+              size: 80,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+            //
+          ],
+        ),
+      ),
+    );
+  }
+}
